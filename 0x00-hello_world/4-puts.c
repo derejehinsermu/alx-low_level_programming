97497ce86ff1e22728fc8910bc020ae12847461d
derejehinsermu <derejehinsermu2@gmail.com>
2023-02-16 02:24:04 -0800

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main (void){
-	/* prints the given string */
+/**
+ * Description: main - prints the given string.
+ * Return: 0 if success.
+ */
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
