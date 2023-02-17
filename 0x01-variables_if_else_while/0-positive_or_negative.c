e827013523fc92fabc221896fee9a32c6c4e0936
derejehinsermu <derejehinsermu2@gmail.com>
2023-02-17 11:33:02 -0800

    positive or negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,14 @@
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < )
-	{
-		print("%d is negative\n", n);
-	}
-	else
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
+
