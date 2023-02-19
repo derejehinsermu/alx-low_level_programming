8b71684fc3fae4c5c9c2308b9aaf515f82ca41eb
derejehinsermu <derejehinsermu2@gmail.com>
2023-02-19 12:42:28 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    int i;
-    int j;
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
+	int i;
+	int j;
+	for (i = 0; i <= 8; i++)
 	{
-            putchar('0' + i);
-	    putchar('0' + j);
-	    if (i ==8 && j == 9)
-	    {
-		    break;
-	    }
-	    putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i ==8 && j == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
